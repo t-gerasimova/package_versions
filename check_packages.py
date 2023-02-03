fa9cf5c427b959379364c34a0cb16d8506085e06
Tatiana Gerasimova <86408205+t-gerasimova@users.noreply.github.com>
2023-02-03 22:43:42 +0300

    Update check_packages.py

--- check_packages.py
+++ check_packages.py
@@ -5,15 +5,21 @@ from os import path
 from pandas import DataFrame
 from openpyxl import load_workbook
 
-raw_list = open('package_list.txt', 'r').readlines()                      # читаем файл с перечнем ПО
-soft_list = [line.rstrip('\n') for line in raw_list]                      # создаем список из перечня ПО
+packages = './package_list.txt'
+src = './tmp/*.json'
+excel_file = 'results.xlsx'
 
-DataFrame({'Name': [], 'Packages': []}).to_excel('results.xlsx', index=False)  # создаем файл excel
+raw_list = open(packages, 'r').readlines()                                 # читаем файл с перечнем ПО
+soft_list = [line.rstrip('\n') for line in raw_list]                       # создаем список из перечня ПО
 
-wb = load_workbook('results.xlsx')                                         # загружаем книгу excel
+
+DataFrame({'Name': [], 'Packages': []}).to_excel(excel_file, index=False)  # создаем файл excel
+
+wb = load_workbook(excel_file)                                             # загружаем книгу excel
 ws = wb.active                                                             # активируем лист excel
 
-files = glob.glob('/tmp/*.json')                                           # находим все файлы json
+files = glob.glob(src)                                                     # находим все файлы json
+
 
 for file in files:
 
@@ -26,6 +32,4 @@ for file in files:
 
     ws.append([name, str(pack)])                                           # добавляем строку данных в excel
 
-wb.save('results.xlsx')                                                    # сохраняем данные в excel
-
-
+wb.save(excel_file)                                                        # сохраняем данные в excel
